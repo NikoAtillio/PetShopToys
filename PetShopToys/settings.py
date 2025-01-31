c933179eaa2eb06f1f58193e775af75542276869
alice173 <137534053+alice173@users.noreply.github.com>
2025-01-31 14:32:43 +0000

    add database

--- PetShopToys/settings.py
+++ PetShopToys/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,13 +67,16 @@ WSGI_APPLICATION = 'PetShopToys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URL = os.environ.get('DATABASE_URL', "postgresql://neondb_owner:2i3lysDwVYoB@ep-billowing-violet-a2y3sv0k.eu-central-1.aws.neon.tech/mouth_widow_chair_401987")
