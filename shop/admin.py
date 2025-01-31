89953d0ec296bac7a1d7d5ed10de447cde47c356
Niko <nikos_norman87@hotmail.com>
2025-01-31 14:37:35 +0000

    models fix

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, PetType
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -12,4 +12,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    
+@admin.register(PetType)
+class PetTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
