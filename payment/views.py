697f5c05c4732234bd8ea53400be3fef86d6a117
Ben <benwilliamscomapi@outlook.com>
2025-02-04 11:17:13 +0000

    Add product pricing and images to checkout page

--- payment/views.py
+++ payment/views.py
@@ -12,27 +12,94 @@ def checkout(request):
     cancelurl = 'payment/cancel/'
    
     checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': '1QoQDDBTWfUSHSrGSu3cExD2',
-                    'quantity': 1,
-                },
-            ],
-            mode='payment',
-            success_url=successurl,
-            cancel_url=cancelurl,
+        line_items=[
+            {
+                # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                'price': 'price_1QoixVBTWfUSHSrGWhGQ0ESZ',
+                'quantity': 1,
+            },
+            {
+                'price': '1QoixzBTWfUSHSrGy6z8jKGd',
+                'quantity': 1,
+            },
+            {
+                'price': '1QoiyaBTWfUSHSrGNOKhuGg4',
+                'quantity': 1,
+            },
+            {
+                'price': '1Qoiz3BTWfUSHSrGrCiQZUKG',
+                'quantity': 1,
+            },
+            {
+                'price': '1QoizaBTWfUSHSrGWyQODZnm',
+                'quantity': 1,
+            },
+            {
+                'price': '1QoizuBTWfUSHSrGbVwiYAHe',
+                'quantity': 1,
+            },
+            {
+                'price': '1Qoj0LBTWfUSHSrGp5It98mk',
+                'quantity': 1,
+            },
+            {
+                'price': '1Qoj0oBTWfUSHSrGbeNZmz78',
+                'quantity': 1,
+            },
+            {
+                'price': '1Qoj1CBTWfUSHSrGP8dAIMem',
+                'quantity': 1,
+            },
+            {
+                'price': '1QojgvBTWfUSHSrG5wxIP3Wg',
+                'quantity': 1,
+            },
+            {
+                'price': '1QojhzBTWfUSHSrGxkQnA8Mq',
+                'quantity': 1,
+            },
+            {
+                'price': '1QojiIBTWfUSHSrGYvmG6uQr',
+                'quantity': 1,
+            },
+            {
+                'price': '1QojipBTWfUSHSrG8mWpAROi',
+                'quantity': 1,
+            },
+            {
+                'price': '1Qojj4BTWfUSHSrGQ9OgKNBl',
+                'quantity': 1,
+            },
+            {
+                'price': '1QojjbBTWfUSHSrG1S1QNCbk',
+                'quantity': 1,
+            },
+            {
+                'price': '1QojjwBTWfUSHSrGYT3n5rxC',
+                'quantity': 1,
+            },
+            {
+                'price': '1QojkJBTWfUSHSrGpeRmCz2S',
+                'quantity': 1,
+            },
+            {
+                'price': '1QojkkBTWfUSHSrGRF5pH6Y3',
+                'quantity': 1,
+            },
+        ],
+        mode='payment',
+        success_url=successurl,
+        cancel_url=cancelurl,
     )
 
-    return HttpResonse('test')
+    return redirect(checkout_session.url)
 
 
-
-def success (request):
+def success(request):
     return render(request, 'payment/success.html')
 
 
-def cancel (request):
+def cancel(request):
     return render(request, 'payment/cancel.html')
 
 
