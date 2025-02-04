e575f454fb6f0945eeff75756ac2eef5e575c7a0
Niko <nikos_norman87@hotmail.com>
2025-02-04 12:24:56 +0000

    Refactor payment flow and templates; update URLs and add Product model

--- payment/models.py
+++ payment/models.py
@@ -36,3 +36,9 @@ class UserPayment(models.Model):
     
     # class Meta:
     #     ordering = ['-payment__payment_date']
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(max_length=200)
--- payment/views.py
+++ payment/views.py
@@ -1,106 +1,49 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.urls import reverse
 import stripe
 from django.conf import settings
-# Create your views here.
-
+from .models import Product
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def checkout(request):
-    successurl = 'payment/success/'
-    cancelurl = 'payment/cancel/'
+    if request.method == "POST":
+        product_id = request.POST.get('product_id')
+        product = get_object_or_404(Product, id=product_id)
+        
+        successurl = request.build_absolute_uri(reverse('success'))
+        cancelurl = request.build_absolute_uri(reverse('cancel'))
    
-    checkout_session = stripe.checkout.Session.create(
-        line_items=[
-            {
-                # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                'price': 'price_1QoixVBTWfUSHSrGWhGQ0ESZ',
-                'quantity': 1,
-            },
-            {
-                'price': '1QoixzBTWfUSHSrGy6z8jKGd',
-                'quantity': 1,
-            },
-            {
-                'price': '1QoiyaBTWfUSHSrGNOKhuGg4',
-                'quantity': 1,
-            },
-            {
-                'price': '1Qoiz3BTWfUSHSrGrCiQZUKG',
-                'quantity': 1,
-            },
-            {
-                'price': '1QoizaBTWfUSHSrGWyQODZnm',
-                'quantity': 1,
-            },
-            {
-                'price': '1QoizuBTWfUSHSrGbVwiYAHe',
-                'quantity': 1,
-            },
-            {
-                'price': '1Qoj0LBTWfUSHSrGp5It98mk',
-                'quantity': 1,
-            },
-            {
-                'price': '1Qoj0oBTWfUSHSrGbeNZmz78',
-                'quantity': 1,
-            },
-            {
-                'price': '1Qoj1CBTWfUSHSrGP8dAIMem',
-                'quantity': 1,
-            },
-            {
-                'price': '1QojgvBTWfUSHSrG5wxIP3Wg',
-                'quantity': 1,
-            },
-            {
-                'price': '1QojhzBTWfUSHSrGxkQnA8Mq',
-                'quantity': 1,
-            },
-            {
-                'price': '1QojiIBTWfUSHSrGYvmG6uQr',
-                'quantity': 1,
-            },
-            {
-                'price': '1QojipBTWfUSHSrG8mWpAROi',
-                'quantity': 1,
-            },
-            {
-                'price': '1Qojj4BTWfUSHSrGQ9OgKNBl',
-                'quantity': 1,
-            },
-            {
-                'price': '1QojjbBTWfUSHSrG1S1QNCbk',
-                'quantity': 1,
-            },
-            {
-                'price': '1QojjwBTWfUSHSrGYT3n5rxC',
-                'quantity': 1,
-            },
-            {
-                'price': '1QojkJBTWfUSHSrGpeRmCz2S',
-                'quantity': 1,
-            },
-            {
-                'price': '1QojkkBTWfUSHSrGRF5pH6Y3',
-                'quantity': 1,
-            },
-        ],
-        mode='payment',
-        success_url=successurl,
-        cancel_url=cancelurl,
-    )
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'product_data': {
+                            'name': product.name,
+                        },
+                        'unit_amount': int(product.price * 100),  # Stripe expects the amount in cents
+                    },
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=successurl,
+            cancel_url=cancelurl,
+        )
 
-    return redirect(checkout_session.url)
+        return redirect(checkout_session.url)
+    else:
+        return redirect('productscat')  # or another relevant page
 
 
 def success(request):
-    return render(request, 'payment/success.html')
+    return render(request, 'success.html')
 
 
 def cancel(request):
-    return render(request, 'payment/cancel.html')
+    return render(request, 'cancel.html')
 
 
 def test(request):
