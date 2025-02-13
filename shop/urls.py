8c2b7ffa1eae0654f6e922d4a9a8ecad7eaf51e8
alice173 <137534053+alice173@users.noreply.github.com>
2025-02-13 10:36:55 +0000

    fix all products url + add link to nav bar

--- shop/urls.py
+++ shop/urls.py
@@ -4,12 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.shop_home, name='shop_home'),
-    
+    path('products/', views.product_list, name='products'),
     path('<slug:pet_type_slug>/', views.product_list, name='product_list_by_pet_type'),
     path('<slug:pet_type_slug>/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-    path('product/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('products/', views.product_list, name='products'),
-    path('productscat/', views.productscat, name='productscat'),
-    path('productsdog/', views.productsdog, name='productsdog'),
+    
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+
     path('checkout/', views.checkout, name='checkout'),
 ]
